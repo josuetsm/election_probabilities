e12a6c0d3235b5dfb1a89fb5a494448add3a48cf
Josue Tapia <josue.tapia@ug.uchile.cl>
2021-05-26 10:20:10 -0400

    Make raw data dir

--- get_data.py
+++ get_data.py
@@ -8,5 +8,9 @@ bucket_files = list_blobs(bucket_name, pattern='VOTACION_8_')
 
 len(bucket_files)
 
+raw_path = 'data/raw'
+if not os.path.exists(raw_path):
+    os.makedirs(raw_path)
+
 for file in bucket_files:
     download_file(bucket_name, name=file, filename=file[1:])
